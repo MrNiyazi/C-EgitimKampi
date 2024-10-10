fc91c2ac3fa7d7b5b9e75e3401742ce2dcc001f9
Berat Aktas <berataktas2834@gmail.com>
2024-10-10 19:26:24 +0300

    While

--- 04_Loops/Program.cs
+++ 04_Loops/Program.cs
@@ -64,16 +64,69 @@ namespace _04_Loops
             //Console.WriteLine("----------------");
             //Console.WriteLine(totalValue);
 
-            int count = 0;
-            for (int i = 1; i < 50; i++)
-            {
-                if(i%7 == 0)
-                {
-                    count++;
-                }
-               
-            }
-            Console.WriteLine(count);
+            //int count = 0;
+            //for (int i = 1; i < 50; i++)
+            //{
+            //    if(i%7 == 0)
+            //    {
+            //        count++;
+            //    }
+
+            //}
+            //Console.WriteLine(count);
+
+            //int bacterium = 1;
+            //for (int i= 1; i <= 24; i++)
+            //{
+            //    bacterium *= 22;
+            //    Console.WriteLine(i + ".Saat Sonunda:" + bacterium);
+            //}
+            #endregion
+
+            #region While döngüsü
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine("Merhaba Döngüler");
+            //    i++;
+            //}
+
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+            //int i = 1;
+            //int sum = 0;
+            //while (i <= 10)
+            //{
+            //    sum += i;
+            //    i++;
+            //}
+            //Console.WriteLine(sum);
+
+            #endregion
+
+            #region Kalvyeden girilen 3 basamaklı sayı
+            //Console.Write("Sayıyı Giriniz:");
+            //int number = int.Parse(Console.ReadLine());
+            //int ones, tens, hunders;
+            //int sum;
+
+            //ones = number % 10;
+            //tens = (number % 100) / 10;
+            //hunders = number / 100;
+
+            //sum = ones + tens + hunders;
+
+            //Console.WriteLine(sum);
+
+
             #endregion
             Console.ReadLine();
         }
